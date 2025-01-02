c3b66f94584ad79fd5cf3ed7664d771677639729
toufikunsure-4529 <toufiksk.ab3@gmail.com>
2025-01-03 02:40:35 +0530

    nav bar close button added navbar container mobile screen full viewport show

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,5 +1,15 @@
 // Navbar Toggle Functionality
-document.getElementById("navbarToggle").addEventListener("click", function () {
+const navbarToggleBtn = document.getElementById("navbarToggle");
+
+navbarToggleBtn.addEventListener("click", function () {
   const mobileMenu = document.getElementById("mobileMenu");
   mobileMenu.classList.toggle("open");
+  const icon = navbarToggleBtn.querySelector("i");
+  if (mobileMenu.classList.contains("open")) {
+    icon.classList.remove("bi-justify-left");
+    icon.classList.add("bi-x-lg");
+  } else {
+    icon.classList.remove("bi-x-lg");
+    icon.classList.add("bi-justify-left");
+  }
 });
